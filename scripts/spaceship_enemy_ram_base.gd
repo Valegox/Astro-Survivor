d4a52ea0883b52e4ca79a3dee43daf3aaf2ba741
Valegox <valentin.gegoux@epitech.eu>
2025-10-30 17:21:31 +0100

    add waves

--- scripts/spaceship_enemy_ram_base.gd
+++ scripts/spaceship_enemy_ram_base.gd
@@ -1,8 +1,6 @@
 extends SpaceshipBase
 class_name SpaceshipEnemyRamBase
 
-@onready var player: SpaceshipPlayer = %SpaceshipPlayer
-
 func _ready() -> void:
 	move_speed = 120
 
@@ -11,10 +9,10 @@ func _physics_process(delta: float) -> void:
 	_process_rotation()
 	
 func _process_movement() -> void:
-	var movement = player.position - position
+	var movement = Player.position - position
 	velocity = movement.normalized() * move_speed
 	move_and_slide()
 
 func _process_rotation() -> void:
-	var angle = (player.position - position).angle()
+	var angle = (Player.position - position).angle()
 	rotation = PI / 2 + angle
