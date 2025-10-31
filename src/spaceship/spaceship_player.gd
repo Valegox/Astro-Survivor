546c9d98550c76109ba488e94e7f98b952333d1a
Volvie <baptiste.laran@epitech.eu>
2025-10-31 14:21:29 +0100

    Add level up animation

--- src/spaceship/spaceship_player.gd
+++ src/spaceship/spaceship_player.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	process_movement()
 	process_rotation()
+	process_level()
 
 func process_movement() -> void:
 	var movement = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
@@ -25,5 +26,18 @@ func process_rotation() -> void:
 	var angle = (mouse_pos - position).angle()
 	var target_rotation = PI / 2 + angle
 	rotation = lerp_angle(rotation, target_rotation, rot_damping)
+
+func process_level() -> void:
+	if xp >= 50:
+		level += 1
+		max_health += 10
+		damage += 10
+		attack_speed += 20
+		xp = 0
+		var xp_anim = $XpAnim
+		xp_anim.show()
+		xp_anim.play()
+		await xp_anim.animation_finished
+		xp_anim.hide()
 
 		
--- src/xp_shard.gd
+++ src/xp_shard.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 class_name XpShard
 
+
 const xp_value = 25
 
 var initial_pos: Vector2
@@ -13,8 +14,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		var player: SpaceshipBase = body
 		if player is SpaceshipPlayer:
 			player.xp += xp_value
-			if player.xp >= 50:
-				player.level += 1
 			self.queue_free()
 			print(player.xp)
 			print(player.level)
